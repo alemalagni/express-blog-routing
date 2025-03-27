5aca2f925c9f5b3eb43b1d61bc1c487971ef4e9d
alemalagni <alemalagni@gmail.com>
2025-03-27 16:50:07 +0100

    feat: add app.js express static

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
+app.use( express.static('public') );
+
 // Index
 app.get( '/posts', (req, res) => {
     res.send('Lista dei post');
