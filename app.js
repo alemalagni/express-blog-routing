235e6ff6fb1910cf6bbea9428f820c8cbf4b3813
alemalagni <alemalagni@gmail.com>
2025-03-27 16:33:23 +0100

    fix

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const app = express();
 const port = 3000;
 
 // Index
-app.get( '/posts/', (req, res) => {
+app.get( '/posts', (req, res) => {
     res.send('Lista dei post');
 });
 
@@ -13,7 +13,7 @@ app.get( '/posts/:id', (req, res) => {
 });
 
 // Store
-app.post( '/posts/', (req, res) => {
+app.post( '/posts', (req, res) => {
     res.send('Creazione del post');
 });
 
